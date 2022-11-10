ac9a055cee19b8d1bf1d455509de2791f879c051
Angshuman Barpujari <angshuarin@gmail.com>
2022-11-10 21:58:50 +0530

    fix: added scrolling

--- lib/features/reading/presentation/pages/reading/reading_page.dart
+++ lib/features/reading/presentation/pages/reading/reading_page.dart
@@ -16,7 +16,7 @@ class ReadingPage extends StatelessWidget {
       create: (context) => SelectedItemReadingCubit(),
       child: Scaffold(
           body: SizedBox(
-            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
             child: Stack(
               children: [
                 SizedBox(
@@ -26,84 +26,122 @@ class ReadingPage extends StatelessWidget {
                     painter: AppBarCustomPainter(),
                   ),
                 ),
-                Positioned(
-                  top: 120,
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        width: MediaQuery.of(context).size.width-40,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(23),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Color.fromRGBO(158, 158, 158, 0.05),
-                              offset: Offset(0, 4),
-                              blurRadius: 4
-                            )
-                          ]
-                        ),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 27, vertical: 30),
-                              child: Row(
-                                children: [
-                                  Image.asset(
-                                    "assets/icons/noto_potted-plant.png",
-                                    height: 32,
+                Padding(
+                  padding: const EdgeInsets.only(top: 120, left: 20, right: 20),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.vertical(top: Radius.circular(23)),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width-40,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(23),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromRGBO(158, 158, 158, 0.05),
+                                  offset: Offset(0, 4),
+                                  blurRadius: 4
+                                )
+                              ]
+                            ),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 27, vertical: 30),
+                                  child: Row(
+                                    children: [
+                                      Image.asset(
+                                        "assets/icons/noto_potted-plant.png",
+                                        height: 32,
+                                      ),
+                                      const SizedBox(
+                                        width: 11,
+                                      ),
+                                      const Text(
+                                        "Soil Description",
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w500
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                  const SizedBox(
-                                    width: 11,
+                                ),
+                                const Divider(
+                                  height: 1,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                SizedBox(
+                                  child: ListView.separated(
+                                    shrinkWrap: true,
+                                    padding: EdgeInsets.zero,
+                                    primary: false,
+                                    itemBuilder: (context, index) {
+                                      return BlocBuilder<SelectedItemReadingCubit, SelectedItemReadingState>(
+                                        builder: (context, state) {
+                                          return ReadingItem(isSelected: state.at==index, index: index,);
+                                        },
+                                      );
+                                    }, 
+                                    separatorBuilder: (context, index) {
+                                      return const Divider(
+                                        indent: 20,
+                                      );
+                                    }, 
+                                    itemCount: 5
                                   ),
-                                  const Text(
-                                    "Soil Description",
-                                    style: TextStyle(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w500
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            const Divider(
-                              height: 1,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            ListView.separated(
-                              shrinkWrap: true,
-                              padding: EdgeInsets.zero,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemBuilder: (context, index) {
-                                return BlocBuilder<SelectedItemReadingCubit, SelectedItemReadingState>(
-                                  builder: (context, state) {
-                                    return ReadingItem(isSelected: state.at==index, index: index,);
-                                  },
-                                );
-                              }, 
-                              separatorBuilder: (context, index) {
-                                return const Divider(
-                                  indent: 20,
-                                );
-                              }, 
-                              itemCount: 3
-                            ),
-                            const SizedBox(
-                              height: 10,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                )
+                              ],
                             )
-                          ],
-                        )
-                      )
-                    ],
+                          ), 
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          _buildPsuedoText( "Gardens are not made by singing ‘Oh, how beautiful,’ and sitting in the shade. – Rudyard Kipling", context),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          Center(
+                            child: _buildPsuedoText("© Team Somadhan", context),
+                          ),
+                          const SizedBox(
+                            height: 60,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 )
               ],
             ),
           ),
         ),
+    );
+  }
+
+  Widget _buildPsuedoText(String text, ctx){
+    return SizedBox(
+      width: MediaQuery.of(ctx).size.width,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 42),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Color.fromRGBO(182, 182, 182, 1),
+            fontSize: 10,
+            fontWeight: FontWeight.w500
+          ),
+        ),
+      ),
     );
   }
 }
