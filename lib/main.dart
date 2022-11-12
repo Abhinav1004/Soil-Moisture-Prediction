df60ee909f7acc567cc25a93bb218467aa368861
Angshuman Barpujari <angshuarin@gmail.com>
2022-11-13 01:46:00 +0530

    added bloc listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rice_disease_detect/core/observer/custom_bloc_observer.dart';
+import 'package:rice_disease_detect/features/examine/presentation/pages/examine/examine_page.dart';
+import 'package:rice_disease_detect/features/home/data/repository/image_input_repository.dart';
+import 'package:rice_disease_detect/features/home/presentation/bloc/image_input_cubit.dart';
+import 'package:rice_disease_detect/features/home/presentation/components/examine_crop_area.dart';
 import 'package:rice_disease_detect/features/home/presentation/pages/home/home_page.dart';
 import 'package:rice_disease_detect/features/reading/presentation/pages/reading/reading_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = CustomBlocObserver();
   runApp(const MyApp());
 }
 
@@ -17,7 +25,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/": (_)=> const HomePage(),
-        "/reading": (_)=> const ReadingPage()
+        "/reading": (_)=> const ReadingPage(),
+        "/examine": (_)=> const ExaminePage()
       },
     );
   }
